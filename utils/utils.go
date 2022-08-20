b40bf880f4d99889f9917b4e57b8861c0cc0048a
cheveuxdelin <victorba2120@gmail.com>
2022-08-20 10:23:08 -0700

    Added new cipher method

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"strings"
+
+	"github.com/cheveuxdelin/cipher/errors"
+)
+
 func ToUpper[T byte | rune](s T) T {
 	if s >= 'a' && s <= 'z' {
 		return s - ('a' - 'A')
@@ -9,4 +15,20 @@ func ToUpper[T byte | rune](s T) T {
 
 func IsASCIIPrintable[T byte | rune](s T) bool {
 	return s >= ' ' && s <= '~'
+}
+
+func ValidateString(s *string) error {
+	if len(*s) == 0 {
+		return errors.ErrEmptyString
+	}
+	*s = strings.TrimSpace(*s)
+	if len(*s) == 0 {
+		return errors.ErrOnlySpaces
+	}
+	for i := range *s {
+		if !IsASCIIPrintable((*s)[i]) {
+			return errors.ErrStringNotASCII
+		}
+	}
+	return nil
 }
